512a5bf95a7364584a4d0093bbfcdfc071ac3f0f
Trent Nicholson <nicholsontrent@gmail.com>
2024-12-02 12:12:37 +1300

    Speed up part 2 by building the map of occurances in the right side while parsing the data

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -3,8 +3,8 @@ use std::collections::HashMap;
 
 advent_of_code::solution!(1);
 
-fn parse(input: &str) -> (Vec<u32>, Vec<u32>) {
-    input
+pub fn part_one(input: &str) -> Option<u32> {
+    let (left_side, right_side): (Vec<u32>, Vec<u32>) = input
         .lines()
         .map(|line| {
             line.split_ascii_whitespace()
@@ -12,11 +12,7 @@ fn parse(input: &str) -> (Vec<u32>, Vec<u32>) {
                 .collect_tuple()
                 .unwrap()
         })
-        .unzip()
-}
-
-pub fn part_one(input: &str) -> Option<u32> {
-    let (left_side, right_side) = parse(input);
+        .unzip();
 
     let distance = std::iter::zip(
         left_side.into_iter().sorted(),
@@ -28,24 +24,27 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(distance)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    let (left_side, right_side) = parse(input);
+pub fn part_two(input: &str) -> Option<usize> {
+    let mut left_side = vec![];
+    let mut right_side: HashMap<usize, usize> = HashMap::new();
 
-    let mut scores: HashMap<u32, u32> = HashMap::new();
-    for a in &left_side {
-        for b in &right_side {
-            if a == b {
-                scores
-                    .entry(*a)
-                    .and_modify(|score| *score += 1)
-                    .or_insert(1);
-            }
-        }
+    for line in input.lines() {
+        let mut items = line.split_whitespace();
+        left_side.push(items.next().unwrap().parse::<usize>().unwrap());
+        right_side
+            .entry(items.next().unwrap().parse::<usize>().unwrap())
+            .and_modify(|v| {
+                *v += 1;
+            })
+            .or_insert(1);
     }
 
-    let similarities = scores.iter().map(|(item, occurances)| item * occurances);
+    let similarity: usize = left_side
+        .iter()
+        .map(|number| number * right_side.get(number).unwrap_or(&0))
+        .sum();
 
-    Some(similarities.sum())
+    Some(similarity)
 }
 
 #[cfg(test)]
