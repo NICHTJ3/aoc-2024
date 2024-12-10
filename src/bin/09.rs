ad50047db09747f83da7aae96c8904332e2abfa5
Trent Nicholson <nicholsontrent@gmail.com>
2024-12-10 21:34:49 +1300

    Day 9 part 2. I pretty much couldn't reuse anything

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -45,7 +45,69 @@ pub fn part_one(input: &str) -> Option<usize> {
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    let mut disk_map = Vec::new();
+    let mut id = 0;
+    input.chars().enumerate().for_each(|(idx, char)| {
+        let is_file = idx % 2 == 0;
+
+        let size = if char.is_ascii_digit() {
+            char.to_digit(10).unwrap()
+        } else {
+            return;
+        };
+
+        disk_map.push(if is_file {
+            (Some(id), size)
+        } else {
+            (None, size)
+        });
+
+        if is_file {
+            id += 1;
+        }
+    });
+
+    // we can't use a left pointer here because even if we find a can't fit x id we might be able
+    // to fit the next block so we have to check most things anyway
+    let mut right = disk_map.len() - 1;
+    while right > 0 {
+        let (file_block_to_move_id, file_block_to_move_size) = disk_map[right];
+
+        if file_block_to_move_id.is_none() {
+            right -= 1;
+            continue;
+        }
+
+        // If we can find a block that will fit the items size
+        if let Some(spot_to_move_to) = disk_map[0..right]
+            .iter()
+            .position(|&(id, size)| id.is_none() && file_block_to_move_size <= size)
+        {
+            let (_, new_spots_size) = disk_map[spot_to_move_to];
+
+            // Make the swap using the original size of the file block
+            disk_map[spot_to_move_to] = (file_block_to_move_id, file_block_to_move_size);
+            disk_map[right] = (None, file_block_to_move_size);
+
+            // If we have space left over create a new empty block
+            if file_block_to_move_size < new_spots_size {
+                disk_map.insert(
+                    spot_to_move_to + 1,
+                    (None, new_spots_size - file_block_to_move_size),
+                );
+            }
+        }
+        right -= 1;
+    }
+
+    Some(
+        disk_map
+            .iter()
+            .flat_map(|&(id, size)| (0..size).map(move |_| id))
+            .enumerate()
+            .flat_map(|(i, id)| id.map(|id| i * id))
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -61,6 +123,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2858));
     }
 }
